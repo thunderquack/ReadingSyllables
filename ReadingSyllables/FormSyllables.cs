66885b8751a5e4212d7b444a57c50c389a6daf86
Egor Ivanov <e.ivanov@m2trust.de>
2023-01-24 14:33:28 +0100

    generation is ok

--- ReadingSyllables/FormSyllables.cs
+++ ReadingSyllables/FormSyllables.cs
@@ -2,6 +2,7 @@
 {
     public partial class FormSyllables : Form
     {
+        private string prevSyllable = "";
         public FormSyllables()
         {
             InitializeComponent();
@@ -9,18 +10,28 @@
 
         private void FormSyllables_KeyPress(object sender, KeyPressEventArgs e)
         {
-            labelSyllable.Text = "Съешь ещё этих мягких французских булок да выпей чаю Съешь ещё этих мягких французских булок да выпей чаю Съешь ещё этих мягких французских булок да выпей чаю Съешь ещё этих мягких французских булок да выпей чаю Съешь ещё этих мягких французских булок да выпей чаю Съешь ещё этих мягких французских булок да выпей чаю Съешь ещё этих мягких французских булок да выпей чаю";
+            prevSyllable = SyllablesGenerator.GenerateSyllable(2, prevSyllable);
+            labelSyllable.Text = prevSyllable;
 
             while (labelSyllable.Width > ((sender as Form) != null ? (sender as Form).Width : 800))
             {
-                labelSyllable.Font = new Font(labelSyllable.Font.FontFamily, labelSyllable.Font.Size - 0.5f, labelSyllable.Font.Style);
+                labelSyllable.Font = new Font(labelSyllable.Font.FontFamily, labelSyllable.Font.Size - 1f, labelSyllable.Font.Style);
             }
 
-            while (labelSyllable.Width < ((sender as Form) != null ? (sender as Form).Width - 200 : 0))
+            while (labelSyllable.Height > ((sender as Form) != null ? (sender as Form).Height : 800))
             {
-                labelSyllable.Font = new Font(labelSyllable.Font.FontFamily, labelSyllable.Font.Size + 0.5f, labelSyllable.Font.Style);
+                labelSyllable.Font = new Font(labelSyllable.Font.FontFamily, labelSyllable.Font.Size - 1f, labelSyllable.Font.Style);
             }
 
+            while (labelSyllable.Width < ((sender as Form) != null ? (sender as Form).Width - 300 : 0))
+            {
+                labelSyllable.Font = new Font(labelSyllable.Font.FontFamily, labelSyllable.Font.Size + 1f, labelSyllable.Font.Style);
+            }
+
+            while (labelSyllable.Height < ((sender as Form) != null ? (sender as Form).Height - 300 : 0))
+            {
+                labelSyllable.Font = new Font(labelSyllable.Font.FontFamily, labelSyllable.Font.Size + 1f, labelSyllable.Font.Style);
+            }
         }
 
         private void tlPanel_Paint(object sender, PaintEventArgs e)
---
+++ ReadingSyllables/SyllablesGenerator.cs
@@ -0,0 +1,47 @@
+﻿namespace ReadingSyllables
+{
+    internal static class SyllablesGenerator
+    {
+        private static List<char> CONSONANTS = new List<char>() {
+            'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'
+        };
+        private static List<char> VOVELS = new List<char>() {
+            'а', 'е', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я' 
+        };
+        private static Random r = new Random();
+
+        public static string GenerateSyllable(int length, string prevSyllable = "")
+        {
+            switch (length)
+            {
+                case 2:
+                    int idx = r.Next(0, VOVELS.Count - 1);
+                    char vovel = VOVELS[idx];
+                    bool dangerousVovel = false;
+                    if (vovel == 'ы' || vovel == 'ю' || vovel == 'я')
+                    {
+                        dangerousVovel = true;
+                    }
+                    List<char> consonants = CONSONANTS.Take(int.MaxValue).ToList();
+                    consonants.Remove('й');
+                    if (dangerousVovel)
+                    {
+                        consonants.Remove('ж');
+                        consonants.Remove('ч');
+                        consonants.Remove('ш');
+                        consonants.Remove('щ');
+                    }
+                    idx = r.Next(0, consonants.Count - 1);
+                    char consonant = consonants[idx];
+                    string res = ($"{consonant}{vovel}").ToUpper();
+                    if (res == prevSyllable)
+                    {
+                        return GenerateSyllable(length, prevSyllable);
+                    }
+                    return ($"{consonant}{vovel}").ToUpper();
+                default:
+                    return "";
+            }
+        }
+    }
+}

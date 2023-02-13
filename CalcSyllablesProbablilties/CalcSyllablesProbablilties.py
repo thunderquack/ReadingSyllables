7456abeb87e77cd5bb9fbe17c50c2be501e01896
Egor Ivanov <egor.i.ivanov@gmail.com>
2023-02-13 17:34:10 +0100

    calculating syllables

--- CalcSyllablesProbablilties/CalcSyllablesProbablilties.py
+++ CalcSyllablesProbablilties/CalcSyllablesProbablilties.py
@@ -1,4 +1,14 @@
-
 import rusyllab
+from os import listdir
+from os.path import isfile, join
+from collections import Counter
 
+onlyfiles = [f for f in listdir("./Texts") if isfile(join("./Texts", f))]
 
+for f in onlyfiles:
+    file_name = join("./Texts", f)
+    file_content = open(file_name, "r", encoding="utf-8")
+    sx = rusyllab.split_words(file_content.read().lower().split())
+    without_len_one = [x for x in sx if len(x) > 1]
+    u_syllables = Counter(without_len_one)
+    print(u_syllables)

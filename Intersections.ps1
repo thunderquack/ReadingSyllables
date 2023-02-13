0ffb7ab76fe38797c859411294d3d0913070d11d
Egor Ivanov <egor.i.ivanov@gmail.com>
2023-02-13 20:41:19 +0100

    extra file

--- Intersections.ps1
+++
@@ -1,8 +0,0 @@
-$consonants = @('б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ')
-$vovels = @('а', 'е', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я')
-
-$idConosant = Get-Random -Minimum 0 -Maximum $consonants.Length-1
-if ($idConosant)
-{
-    
-}

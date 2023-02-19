a87cc849798a69b42f1e0e43306d2875426728c3
Egor Ivanov <egor.i.ivanov@gmail.com>
2023-02-19 21:38:43 +0200

    rating creation

--- CalcSyllablesProbablilties/MakeWordList.py
+++ CalcSyllablesProbablilties/MakeWordList.py
@@ -10,14 +10,21 @@ words = open(words_file_name, "r", encoding="utf-8").read().splitlines()
 syllables_file_name = './result.json'
 syllables = json.load(open(syllables_file_name, 'r', encoding='utf-8'))
 
+words_list = {}
+
 for word in words:
     sx = rusyllab.split_word(word.lower())
-    rating = len(syllables) + 1
+    rating = 0
     for syllable in sx:
-        element = next(s for s in syllables if s['name']==syllable)
-        if (element['id']<rating):
+        element = next((s for s in syllables if s['name']==syllable), 0)
+        if element != 0 and element['id']>rating:
             rating = element['id']
-    print(word, rating)
+    if rating != 0:
+        words_list[word] = rating
+words_list = sorted(words_list.items(), key=lambda item: item[1])
 
+words_file_name = './rating.json'
+words = open(words_file_name, "w", encoding="utf-8")
+json.dump(words_list, words)
 
 print('finished')

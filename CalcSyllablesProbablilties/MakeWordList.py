3a4a7dce35215b28b27747f40606308319c2de1b
Egor Ivanov <egor.i.ivanov@gmail.com>
2023-02-19 11:11:42 +0200

    rating calc

--- CalcSyllablesProbablilties/MakeWordList.py
+++ CalcSyllablesProbablilties/MakeWordList.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 import rusyllab  # https://github.com/Koziev/rusyllab
 from os import listdir
 from os.path import isfile, join
@@ -8,5 +9,15 @@ words_file_name = './Words/RussianWords.txt'
 words = open(words_file_name, "r", encoding="utf-8").read().splitlines()
 syllables_file_name = './result.json'
 syllables = json.load(open(syllables_file_name, 'r', encoding='utf-8'))
+
+for word in words:
+    sx = rusyllab.split_word(word.lower())
+    rating = len(syllables) + 1
+    for syllable in sx:
+        element = next(s for s in syllables if s['name']==syllable)
+        if (element['id']<rating):
+            rating = element['id']
+    print(word, rating)
+
 
 print('finished')

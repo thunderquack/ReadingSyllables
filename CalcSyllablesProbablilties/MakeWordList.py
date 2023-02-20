6a41bd3558e612c7b176473ab56b0fe32d5ae545
Egor Ivanov <egor.i.ivanov@gmail.com>
2023-02-20 11:46:41 +0200

    formatting

--- CalcSyllablesProbablilties/MakeWordList.py
+++ CalcSyllablesProbablilties/MakeWordList.py
@@ -5,10 +5,10 @@ from os.path import isfile, join
 from collections import Counter
 import json
 
-words_file_name = './Words/RussianWords.txt'
+words_file_name = "./Words/RussianWords.txt"
 words = open(words_file_name, "r", encoding="utf-8").read().splitlines()
-syllables_file_name = './result.json'
-syllables = json.load(open(syllables_file_name, 'r', encoding='utf-8'))
+syllables_file_name = "./result.json"
+syllables = json.load(open(syllables_file_name, "r", encoding="utf-8"))
 
 words_list = {}
 
@@ -16,15 +16,15 @@ for word in words:
     sx = rusyllab.split_word(word.lower())
     rating = 0
     for syllable in sx:
-        element = next((s for s in syllables if s['name']==syllable), 0)
-        if element != 0 and element['id']>rating:
-            rating = element['id']
+        element = next((s for s in syllables if s["name"] == syllable), 0)
+        if element != 0 and element["id"] > rating:
+            rating = element["id"]
     if rating != 0:
         words_list[word] = rating
 words_list = sorted(words_list.items(), key=lambda item: item[1])
 
-words_file_name = './rating.json'
+words_file_name = "./rating.json"
 words = open(words_file_name, "w", encoding="utf-8")
 json.dump(words_list, words)
 
-print('finished')
+print("finished")

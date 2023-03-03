69910d1a55c126c27e787618520f5b634f566ec5
Egor Ivanov <egor.i.ivanov@gmail.com>
2023-03-03 13:28:39 +0100

    recalc is complete

--- CalcSyllablesProbablilties/CreateSyllablesFromNouns.py
+++ CalcSyllablesProbablilties/CreateSyllablesFromNouns.py
@@ -1,6 +1,4 @@
 import rusyllab  # https://github.com/Koziev/rusyllab
-from os import listdir
-from os.path import isfile, join
 from collections import Counter
 import json
 
@@ -14,30 +12,11 @@ sx = rusyllab.split_words(words.keys())
 without_len_one = [x for x in sx if len(x) > 1]
 u_syllables = Counter(without_len_one)
 
-for w in words:
-    sx = rusyllab.split_words(w)
-    without_len_one = [x for x in sx if len(x) > 1]
-    u_syllables = u_syllables + Counter(without_len_one)
-
-onlyfiles = [f for f in listdir("./Texts") if isfile(join("./Texts", f))]
-
-#syllables_to_remove = ["князь", "княж"]
-
-for f in onlyfiles:
-    file_name = join("./Texts", f)
-    print('file name', file_name)
-    file_content = open(file_name, "r", encoding="utf-8")
-    sx = rusyllab.split_words(file_content.read().lower().split())
-    without_len_one = [x for x in sx if len(x) > 1]
-    u_syllables = u_syllables + Counter(without_len_one)
-
-for ignore in syllables_to_remove:
-    del u_syllables[ignore]
-
-f = open("result.json", "w")
+f = open("result.json", "w", encoding="utf-8")
 f.write(
     json.dumps(
-        [{"id":idx+1, "name": element[0], "value": element[1]} for idx, element in enumerate(u_syllables.most_common())]
+        [{"id":idx+1, "name": element[0], "value": element[1]} for idx, element in enumerate(u_syllables.most_common())],
+        ensure_ascii=False
     )
 )
 f.close()

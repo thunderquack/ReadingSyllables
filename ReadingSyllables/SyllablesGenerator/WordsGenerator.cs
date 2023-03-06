6784151c430c75102eb3428673225de778154f31
Egor Ivanov <egor.i.ivanov@gmail.com>
2023-03-06 20:25:19 +0100

    word generator stub

---
+++ ReadingSyllables/SyllablesGenerator/WordsGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace ReadingSyllables.SyllablesGenerator
+{
+    internal class WordsGenerator : AbstractGenerator
+    {
+        public override string GenerateSyllable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetShortSettings()
+        {
+            throw new NotImplementedException();
+        }
+        public WordsGenerator(Settings settings) : base(settings)
+        {
+        }
+    }
+}

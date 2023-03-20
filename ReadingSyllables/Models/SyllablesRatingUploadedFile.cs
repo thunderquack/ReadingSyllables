106e099432ef362a4dd780800329a161e9ba48a9
Egor Ivanov <egor.i.ivanov@gmail.com>
2023-03-20 16:57:06 +0100

    new hashing structure stub

---
+++ ReadingSyllables/Models/SyllablesRatingUploadedFile.cs
@@ -0,0 +1,7 @@
+﻿namespace ReadingSyllables.Models
+{
+    internal class SyllablesRatingUploadedFile : UploadedFile
+    {
+        public new UploadedFileType UploadedFileType { get; set; } = UploadedFileType.SyllablesRating;
+    }
+}
---
+++ ReadingSyllables/Models/UploadedFile.cs
@@ -0,0 +1,10 @@
+﻿namespace ReadingSyllables.Models
+{
+    internal abstract class UploadedFile
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string HashSum { get; set; }
+        public UploadedFileType UploadedFileType { get; set; }
+    }
+}
